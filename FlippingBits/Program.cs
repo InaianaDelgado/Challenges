53d30231e2ddc2930aff27a8bef6cbd0462c79ec
Inaiana Delgado <inaianadelgado7@gmail.com>
2024-09-01 18:20:47 -0300

    LonelyInteger challenge was adding

---
+++ FlippingBits/Program.cs
@@ -0,0 +1,24 @@
+﻿class FlippingBits
+{
+    public static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+            long result = FlippingB(n);
+
+            Console.WriteLine(result);
+        }
+
+        Console.ReadKey();
+    }
+
+    public static long FlippingB(long n)
+    {
+        //work in progress
+        return 0;
+    }
+}
--- LonelyInteger/Program.cs
+++ LonelyInteger/Program.cs
@@ -2,17 +2,43 @@
 {
     public static void Main(string[] args)
     {
+        Console.Write("");
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
+        if (n < 1 || n > 100)
+            return;
+
+        if ((n % 2) == 0)
+            return;
+
+        Console.Write("");
         List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        if (a.Count < 0 || a.Count > n)
+            return;
 
         int result = Lonelyinteger(a);
 
         Console.WriteLine(result);
+        Console.ReadKey();
     }
 
     public static int Lonelyinteger(List<int> a)
     {
-        return 0;
+        int lonely = 0;
+        int counter = 0;
+
+        for (int i = 0; i < a.Count; i++)
+        {
+            for (int j = 0; j < a.Count; j++)
+                if (a[i] == a[j]) counter++;     
+
+            if (counter == 1)
+                lonely = a[i];
+
+            counter = 0;
+        }
+
+        return lonely;
     }
 }
